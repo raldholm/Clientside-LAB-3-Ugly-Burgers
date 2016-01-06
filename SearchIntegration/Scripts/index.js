685a5470e399b8c109eca4dc9cdcfe8cda13ed83
Martin Raldholm <martin@raldholm.se>
2016-01-06 20:20:22 +0100

    Added description for hamburgers. Added search field.

--- SearchIntegration/Scripts/index.js
+++ SearchIntegration/Scripts/index.js
@@ -1,25 +1,28 @@
 ﻿$(document).ready(function () {
 
-    function getHamburgersFunction() { var burgers = $("#burgersPlace").val();
+    function getHamburgersFunction() {
+        var burgers = $("#burgersPlace").val();
         var meExp = new RegExp(burgers, "i");
-        $.getJSON('hamburgers.json', function(data) {
+        $.getJSON('hamburgers.json', function (data) {
             var output = "<ul>";
             $.each(data, function (key, val) {
                 //Sortering
                 if ((val.name.search(meExp) !== -1)) {
                     output += "<li>";
-                    output += "<p>" + "<b>" + "Burger dish: "+ "</b>" + val.name + "</p>";
+                    output += "<p>" + "<b>" + val.name + "</b>" + "</p>";
+                    output += "<p>" + val.description + "</p>";
                     output += "<p>" + "<b>" + "Size: " + "</b>" + val.size + "</p>";
                     output += "<p>" + "<b>" + "Day of the week: " + "</b>" + val.dayOfTheWeek + "</p>";
-                    output += "<p>" + "<b>" + "Price: " + "</b>" + "$ "+val.price + "</p>";
+                    output += "<p>" + "<b>" + "Price: " + "</b>" + "$ " + val.price + "</p>";
                     output += "</li>";
                 }
             });
             output += "</ul>";
             $("#burgersPlace").html(output);
-        });};
+        });
+    };
 
-    getHamburgersFunction();
+    //getHamburgersFunction();
 
 
 });
