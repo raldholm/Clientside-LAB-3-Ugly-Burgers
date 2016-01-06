06a0ec3207dc2010e9d98f5b76ad248b0344aa75
Martin Raldholm <martin@raldholm.se>
2016-01-06 19:48:25 +0100

    added and tested getHamburgersFunction()

--- SearchIntegration/Scripts/index.js
+++ SearchIntegration/Scripts/index.js
@@ -1,33 +1,26 @@
 ﻿$(document).ready(function () {
 
-    var burgers = $("#burgersPlace").val();
+    function getHamburgersFunction() { var burgers = $("#burgersPlace").val();
+        var meExp = new RegExp(burgers, "i");
+        $.getJSON('hamburgers.json', function(data) {
+            var output = "<ul>";
+            $.each(data, function (key, val) {
+                //Sortering
+                if ((val.name.search(meExp) !== -1)) {
+                    output += "<li>";
+                    output += "<p>" + "<b>" + "Burger dish: "+ "</b>" + val.name + "</p>";
+                    output += "<p>" + "<b>" + "Size: " + "</b>" + val.size + "</p>";
+                    output += "<p>" + "<b>" + "Day of the week: " + "</b>" + val.dayOfTheWeek + "</p>";
+                    output += "<p>" + "<b>" + "Price: " + "</b>" + "$ "+val.price + "</p>";
+                    output += "</li>";
+                }
+            });
+            output += "</ul>";
+            $("#burgersPlace").html(output);
+        });};
 
-    var meExp = new RegExp(burgers, "i");
+    getHamburgersFunction();
 
-    $.getJSON('products.json', function(data) {
-
-        var output = "<ul>";
-
-        //$.for()
-
-        $.each(data, function (key, val) {
-
-            alert(val.toArray);
-
-            //Sortering
-            if ((val.name.search(meExp) !== -1)) {
-                alert("hej");
-                output += "<li>";
-                output += "<p>" + val.name + "<b> " + val.name + "</b>" + "</p>";
-                output += "<p>" + val.id + "</p>";
-                output += "</li>";
-            }
-
-        });
-        output += "</ul>";
-
-        $("#burgersPlace").html(output);
-    });
 
 });
 
