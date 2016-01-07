802db6e88cbafb1f206c0908dc836c2bb519cfe8
Martin Raldholm <martin@raldholm.se>
2016-01-07 19:21:26 +0100

    re-created

--- SearchIntegration/Scripts/index.js
+++ SearchIntegration/Scripts/index.js
@@ -1,83 +1,105 @@
-﻿$(document).ready(function () {
+﻿//var Products = function (id, name, size, description, price) {
+//    this.id = id;
+//    this.name = name;
+//    this.size = size;
+//    this.description = description;
+//    this.price = price;
+//};
 
-    function getHamburgersFunction() {
+$(document).ready(function () {
 
-        $.getJSON('hamburgers.json', function (data) {
-            var html = "";
-            $.each(data, function (key, val) {
-                html += "<article>";
-                html += "<fieldset>";
-                html += "<legend>" + val.name +  "</legend>";
-                html += "<h3>" + val.dayOfTheWeek + "'s Special!" + "</h3>";
-                html += "<span>"+ "<img src="+'Images/Hamburger.png' +"></span>";
-                html += "<div id='description'>" + val.description + "</div>";
-                html += "<div class='price'>" + val.price + "</div>";
-                html += "</fieldset>";
-                html += "</article>";
-            });
-            html += "";
-            $("#burgersPlace").html(html);
-        });
-    };
-    function getPizzasFunction() {
-        $.getJSON('pizzas.json', function (data) {
-            var html = "";
-            $.each(data, function (key, val) {
-                html += "<article>";
-                html += "<fieldset>";
-                html += "<legend>" + val.name + "</legend>";
-                html += "<h3>" + val.dayOfTheWeek + "'s Special!" + "</h3>";
-                html += "<span>" + "<img src=" + 'Images/Pizza.png' + "></span>";
-                //html += "<div id='description'>" + val.description + "</div>";
-                html += "<div class='price'>" + val.price + "</div>";
-                html += "</fieldset>";
-                html += "</article>";
-            });
-            html += "";
-            $("#pizzaPlace").html(html);
-        });
-    };
-    function getSubsFunction()
-    {
-        $.getJSON('subs.json', function (data) {
-            var html = "";
-            $.each(data, function (key, val) {
-                html += "<article>";
-                html += "<fieldset>";
-                html += "<legend>" + val.name + "</legend>";
-                html += "<h3>" + val.dayOfTheWeek + "'s Special!" + "</h3>";
-                html += "<span>" + "<img src=" + 'Images/Subs.png' + "></span>";
-                //html += "<div id='description'>" + val.description + "</div>";
-                html += "<div class='price'>" + val.price + "</div>";
-                html += "</fieldset>";
-                html += "</article>";
-            });
-            html += "";
-            $("#subsPlace").html(html);
-        });
-    };
-    function getDrinksFunction() {
-        $.getJSON('drinks.json', function (data) {
-            var html = "";
-            $.each(data, function (key, val) {
-                html += "<article>";
-                html += "<fieldset>";
-                html += "<legend>" + val.name + "</legend>";
-                html += "<h3>" + val.dayOfTheWeek + "'s Special!" + "</h3>";
-                html += "<span>" + "<img src=" + 'Images/Drink.png' + "></span>";
-                //html += "<div id='description'>" + val.description + "</div>";
-                html += "<div class='price'>" + val.price + "</div>";
-                html += "</fieldset>";
-                html += "</article>";
-            });
-            html += "";
-            $("#drinksPlace").html(html);
-        });
-    };
+    $('#checkbox_hamburgers').click(function () {
+        if (!$(this).is(':checked')) {
 
-    getHamburgersFunction();
-    getPizzasFunction();
-    getSubsFunction();
-    getDrinksFunction();
+            $("#hamBurgersDiv").addClass('hidden');
+        }     
+    });
 
+    if ($('#checkbox_hamburgers').is(':checked')) {
+        getAllHamburgers();
+    }
+    if ($('#checkbox_pizzas').is(':checked')) {
+        getAllPizzas();
+    }
+    if ($('#checkbox_subs').is(':checked')) {
+        getAllSubs();
+    }
+    if ($('#checkbox_drinks').is(':checked')) {
+        getAllDrinks();
+    }
+    
 });
+
+
+function getAllHamburgers() {
+    $.getJSON('hamburgers.json', function(data) {
+        var output = "";
+        $.each(data, function(key, val) {
+            output += "<div id='hamburger'>";
+            output += "<p>" + "<b>" + val.name + "</b>";
+            output += "<p>" + "Description: " + val.description + " </p>";
+            output += "<img src='Images/Hamburger.png'";
+            output += '"alt="' + val.name + '"' + '"/>';
+            output += "<p>" + "Size: " + val.size + " </p>";
+            output += "<p>" + "Price: " + val.price.toFixed(2) + " </p>";
+            output += "</div>";
+        });
+        output += "";
+        $("#hamBurgersDiv").html(output);
+
+    });
+}
+function getAllPizzas() {
+    $.getJSON('pizzas.json', function (data) {
+        var output = "";
+        $.each(data, function (key, val) {
+            output += "<div id='pizza'>";
+            output += "<p>" + "<b>" + val.name + "</b>";
+            output += "<p>" + "Description: " + val.description + " </p>";
+            output += "<img src='Images/Pizza.png'";
+            output += '"alt="' + val.name + '"' + '"/>';
+            output += "<p>" + "Size: " + val.size + " </p>";
+            output += "<p>" + "Price: " + val.price.toFixed(2) + " </p>";
+            output += "</div>";
+        });
+        output += "";
+        $("#pizzasDiv").html(output);
+
+    });
+}
+function getAllSubs() {
+    $.getJSON('subs.json', function (data) {
+        var output = "";
+        $.each(data, function (key, val) {
+            output += "<div id='sub'>";
+            output += "<p>" + "<b>" + val.name + "</b>";
+            output += "<p>" + "Description: " + val.description + " </p>";
+            output += "<img src='Images/Subs.PNG'";
+            output += '"alt="' + val.name + '"' + '"/>';
+            output += "<p>" + "Size: " + val.size + " </p>";
+            output += "<p>" + "Price: " + val.price.toFixed(2) + " </p>";
+            output += "</div>";
+        });
+        output += "";
+        $("#subsDiv").html(output);
+
+    });
+}
+function getAllDrinks() {
+    $.getJSON('drinks.json', function (data) {
+        var output = "";
+        $.each(data, function (key, val) {
+            output += "<div id='drink'>";
+            output += "<p>" + "<b>" + val.name + "</b>";
+            output += "<p>" + "Description: " + val.description + " </p>";
+            output += "<img src='Images/Drink.png'";
+            output += '"alt="' + val.name + '"' + '"/>';
+            output += "<p>" + "Size: " + val.size + " </p>";
+            output += "<p>" + "Price: " + val.price.toFixed(2) + " </p>";
+            output += "</div>";
+        });
+        output += "";
+        $("#drinksDiv").html(output);
+
+    });
+}
